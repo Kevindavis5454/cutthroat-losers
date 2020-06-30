3c277f356626e6f9d170adc21a3be9b09b2adf3a
Catherine Davis <catherinevtdavis@gmail.com>
2020-06-30 14:25:06 -0600

    contact page finished

--- src/ContactPage/Contact.js
+++ src/ContactPage/Contact.js
@@ -5,30 +5,30 @@ import {Link} from "react-router-dom"
 class Contact extends React.Component {
     render() {
         return(
-            <div>
+            <div className='contact-section'>
                 <h2>Contact us!</h2>
                 <p>For help or questions:</p>
                 <form action="#" method="post" target="_blank">
-                            <p><label htmlFor="name">Your Name</label>
+                            <p><label htmlFor="name">Your Name:</label>
                                 <input type="text"
                                        name="name"
                                        id="name"/>
                             </p>
-                            <p><label htmlFor="question-type">Request type</label>
+                            <p><label htmlFor="question-type">Request type:</label>
                                 <select id="request-type" name="request-type">
                                     <option value="question">Question</option>
                                     <option value="comment">Comment</option>
                                     <option value="bug-report">Bug Report</option>
                                 </select>
                             </p>
-                            <p><label htmlFor="message">Message</label>
+                            <p><label htmlFor="message">Message:</label>
                                 <textarea name="message"></textarea>
                             </p>
                             <p className="submit-wrap">
-                                <Link to='/contact'><input type="submit"
+                                <Link to='/personal/contact'><input type="submit"
                                        id="ftp-submit"
                                        className="button"
-                                       value="Login"/></Link>
+                                       value="Submit"/></Link>
                             </p>
                         </form>
             </div>
