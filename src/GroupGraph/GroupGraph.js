47616bcf3dd259a2856f7930ab13fe675f99b84d
Catherine Davis <catherinevtdavis@gmail.com>
2020-08-12 01:51:42 -0600

    finished data calls for group graph

--- src/GroupGraph/GroupGraph.js
+++ src/GroupGraph/GroupGraph.js
@@ -11,34 +11,26 @@ import { VictoryChart, VictoryStack, VictoryBar, VictoryAxis} from 'victory'
           [
             // contestant 1
               { x: "Weight", y: 1 },
-              { x: "Bingo", y: 2 },
               { x: "Stomach", y: 3 },
-              { x: "Workout", y: 2 },
-              { x: "Admin", y: 2 }
+              { x: "Workout", y: 2 }
           ],
           [
             // contestant 2
               { x: "Weight", y: 1 },
-              { x: "Bingo", y: 2 },
               { x: "Stomach", y: 3 },
-              { x: "Workout", y: 2 },
-              { x: "Admin", y: 2 }
+              { x: "Workout", y: 2 }
           ],
           [
             // contestant 3
               { x: "Weight", y: 1 },
-              { x: "Bingo", y: 2 },
               { x: "Stomach", y: 3 },
-              { x: "Workout", y: 2 },
-              { x: "Admin", y: 2 }
+              { x: "Workout", y: 2 }
           ],
           [
             // contestant 4
               { x: "Weight", y: 2 },
-              { x: "Bingo", y: 3 },
               { x: "Stomach", y: 4 },
-              { x: "Workout", y: 5 },
-              { x: "Admin", y: 2 }
+              { x: "Workout", y: 5 }
           ]
         ],
         bingoPointsData: [],
@@ -56,38 +48,65 @@ import { VictoryChart, VictoryStack, VictoryBar, VictoryAxis} from 'victory'
       return await Promise.all([getContestants()])
   }
 
-      const getBingoPointsInfo = (user_id) => {
-        return fetch(`${config.API_ENDPOINT}/api/points/bingo?user_id=${user_id}`)
+      const getWeightPointsInfo = (user_id) => {
+        return fetch(`${config.API_ENDPOINT}/api/points/weight?user_id=${user_id}`)
             .then(res => res.json())
     }
+    const getStomachPointsInfo = (user_id) => {
+      return fetch(`${config.API_ENDPOINT}/api/points/stomach?user_id=${user_id}`)
+          .then(res => res.json())
+  }
+  const getWorkoutPointsInfo = (user_id) => {
+    return fetch(`${config.API_ENDPOINT}/api/points/workout?user_id=${user_id}`)
+        .then(res => res.json())
+}
 
-    async function getBingoPointStats(user_id) {
-        return await Promise.all([getBingoPointsInfo(user_id)])
+    async function getWeightPointStats(user_id) {
+        return await Promise.all([getWeightPointsInfo(user_id)])
     }
+    async function getStomachPointStats(user_id) {
+      return await Promise.all([getStomachPointsInfo(user_id)])
+  }
+  async function getWorkoutPointStats(user_id) {
+    return await Promise.all([getWorkoutPointsInfo(user_id)])
+}
 
 
     getContestantIds()
       .then(([results]) => {
-        const bingoPointsData = []
+        const weightPointsData = []
+        const stomachPointsData = []
+        const workoutPointsData = []
+        const contestantData0 = []
+        const contestantData1 = []
+        const contestantData2 = []
+        const contestantData3 = []
+        const contestantData= [[],[],[],[]]
         results.forEach(user => {
-          getBingoPointStats(user.user_id)
+          getWeightPointStats(user.user_id)
             .then(([results]) => {
-              bingoPointsData.push(results)
+              weightPointsData.push({ x: 'Weight', y: results[0].sum})
             })
             })
-        this.setState({
-          bingoPointsData: bingoPointsData
+        results.forEach(user => {
+          getStomachPointStats(user.user_id)
+            .then(([results]) => {
+              stomachPointsData.push({ x: 'Stomach', y: results[0].sum })
+            })
         })
-        console.log(this.state.bingoPointsData)
+        results.forEach(user => {
+          getWorkoutPointStats(user.user_id)
+            .then(([results]) => {
+              workoutPointsData.push({ x: 'Workout', y: results[0].sum })
+            })
         })
-
-
+        })
+            
       }
     
 
     render() {
       const dataset = this.state.dataSet;
-      console.log(dataset, "dataset")
       return (
         <div>
         <VictoryChart height={400} width={400}
@@ -104,7 +123,7 @@ import { VictoryChart, VictoryStack, VictoryBar, VictoryAxis} from 'victory'
               tickFormat={(tick) => `${tick}`}
             />
             <VictoryAxis
-              tickFormat={["Weight", "Bingo", "Stomach", "Workout", "Admin"]}
+              tickFormat={["Weight", "Stomach", "Workout"]}
             />
         </VictoryChart>
       </div>
